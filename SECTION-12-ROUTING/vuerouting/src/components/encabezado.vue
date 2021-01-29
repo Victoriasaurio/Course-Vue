329683723ce0ebe70e65c8920ac0421413defed7
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-29 17:29:15 -0600

    Encabezado - Implementación de TO para navegar a la ruta usuarios

--- SECTION-12-ROUTING/vuerouting/src/components/encabezado.vue
+++ SECTION-12-ROUTING/vuerouting/src/components/encabezado.vue
@@ -2,10 +2,20 @@
     <div class="header clearfix">
         <nav>
             <ul class="nav nav-pills pull-right">
-                <li><a href="">Inicio</a></li>
-                <li><a href="">Usuarios</a></li>
+                <router-link tag="li" to="/" class="nav-link active">
+                    <a class="active">Inicio</a>
+                </router-link>
+                <router-link tag="li" to="/usuarios" class="nav-link">
+                    <a>Usuarios</a>
+                </router-link>
             </ul>
             <h3 class="text muted">Aplicación de Usuarios</h3>
         </nav>
     </div>
 </template>
+
+<style scope>
+.active {
+    color: white;
+}
+</style>
