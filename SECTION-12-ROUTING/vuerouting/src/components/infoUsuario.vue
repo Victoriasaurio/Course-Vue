4b5dd768fd9aa6a0752e853d71739a5665d542b6
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-29 17:28:02 -0600

    Información de Usuario - Implementación del botón volver - Implementación del método volver - En caso de contar con las sub-rutas se puede utilizar WATCH, el cual está en escucha del cambio de información.

---
+++ SECTION-12-ROUTING/vuerouting/src/components/infoUsuario.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h3>Información del usuario</h3>
+        <div>
+            <p><strong>Nombre: </strong> {{ nombre }}</p>
+            <p><strong>E-mail: </strong> {{ email }}</p>
+        </div>
+        <button class="btn btn-primary" @click="volver">Volver</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            nombre: '',
+            email: ''
+        }
+    },
+    // Solo es necesario cuando necesitamos actualizar información en una sub-ruta.
+    // watch: { // Mira cuando $route cambia para actualizar la info.
+    //     $route() {
+    //         let id = this.$route.params.id
+    //         this.$http.get('https://jsonplaceholder.typicode.com/users/' + id)
+    //                     .then(respuesta => {
+    //                         return respuesta.json()
+    //                     })
+    //                     .then(usuario => {
+    //                         this.nombre = usuario.name
+    //                         this.email = usuario.email
+    //                     })
+    //     }
+    // },
+    methods: {
+        volver() {
+            // this.$router.push('/usuarios')       // Obteniendo la ruta por medio de una ruta.
+            this.$router.push({ name: 'usuario' }) // Obteniendo la ruta por medio del nombre.
+        }
+    },
+    created() {
+        let id = this.$route.params.id
+        this.$http.get('https://jsonplaceholder.typicode.com/users/' + id)
+                    .then(respuesta => {
+                        return respuesta.json()
+                    })
+                    .then(usuario => {
+                        this.nombre = usuario.name
+                        this.email = usuario.email
+                    })
+    },
+}
+</script>
