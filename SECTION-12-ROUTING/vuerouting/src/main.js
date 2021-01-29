4c81380e0991817ca119fd135e818ed0283234e3
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-29 17:32:52 -0600

    MAIN - Elimina el # - Declaración de VueResource

--- SECTION-12-ROUTING/vuerouting/src/main.js
+++ SECTION-12-ROUTING/vuerouting/src/main.js
@@ -1,17 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import { rutas } from './rutas' //Importación de las rutas
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
 const enrutador = new VueRouter({
-  routes: rutas
+  routes: rutas,
+  mode: 'history' //Eliminación del #
 })
 
 new Vue({
   router: enrutador,
   render: h => h(App),
 }).$mount('#app')
+
+// Para utilizar el mode: 'history' en producción se deben realizar cambios en el servidor para que arroje siempre index.html
